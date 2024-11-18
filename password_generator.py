2f3deeff0ab7fc5690883fc90eb6f99a6bdf1854
onerhook <141339052+onerhook@users.noreply.github.com>
2024-11-18 16:27:52 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,24 @@
+# password_generator.py
+
 import random
 import string
 
 class PasswordGenerator:
-    def __init__(self, length=12, include_digits=True, include_symbols=True):
+    def __init__(self, length=12, use_digits=True, use_special_chars=True):
         self.length = length
-        self.include_digits = include_digits
-        self.include_symbols = include_symbols
+        self.use_digits = use_digits
+        self.use_special_chars = use_special_chars
 
     def generate(self):
-        characters = string.ascii_letters
-        if self.include_digits:
-            characters += string.digits
-        if self.include_symbols:
-            characters += string.punctuation
-        return ''.join(random.choice(characters) for _ in range(self.length))
+        characters = string.ascii_letters  # Используем буквы (верхний и нижний регистр)
+        
+        if self.use_digits:
+            characters += string.digits  # Добавляем цифры
+
+        if self.use_special_chars:
+            characters += string.punctuation  # Добавляем специальные символы
+
+        # Генерация пароля случайным образом из доступных символов
+        password = ''.join(random.choice(characters) for _ in range(self.length))
+        
+        return password
