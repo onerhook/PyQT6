5eda176ef2f84457d81bf91cbb5b180876551402
onerhook <141339052+onerhook@users.noreply.github.com>
2024-11-20 22:47:09 +0300

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,15 @@
-# exceptions.py
-
 class PasswordManagerError(Exception):
-    """Базовый класс для исключений в менеджере паролей."""
+    """базовый класс для исключений в менеджере паролей"""
     pass
+
 
 class DatabaseError(PasswordManagerError):
-    """Исключение для ошибок базы данных."""
+    """исключение для ошибок базы данных"""
     pass
 
+
 class PasswordStrengthError(PasswordManagerError):
-    """Исключение для слабых паролей."""
-    def __init__(self, message="Пароль слишком слабый."):
+    """исключение для слабых паролей"""
+
+    def __init__(self, message="пароль слишком слабый"):
         super().__init__(message)
