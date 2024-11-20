f5c72a2112229902d69047d0c76268f86f732c07
onerhook <141339052+onerhook@users.noreply.github.com>
2024-11-20 22:22:02 +0300

    Update csv_handler.py

--- csv_handler.py
+++ csv_handler.py
@@ -1,4 +1,4 @@
-#csv_handler.py
+# csv_handler.py
 
 import csv
 from db_handler import DBHandler
@@ -7,17 +7,19 @@ class CSVHandler:
     def __init__(self, db_handler):
         self.db_handler = db_handler
 
-    def export_to_csv(self, file_name="passwords.csv"):
+    def export_to_csv(self, file_name="пароли.csv"):
+        """Экспортирует данные из базы данных в CSV-файл."""
         passwords = self.db_handler.get_passwords()
         with open(file_name, "w", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow(["Title", "Username", "Password", "Note"])
+            writer.writerow(["Название", "Логин", "Пароль", "Заметка"])
             writer.writerows(passwords)
 
-    def import_from_csv(self, file_name="passwords.csv"):
+    def import_from_csv(self, file_name="пароли.csv"):
+        """Импортирует данные из CSV-файла в базу данных."""
         with open(file_name, "r") as file:
             reader = csv.reader(file)
-            next(reader)  # Skip header
+            next(reader)  # Пропустить заголовок
             for row in reader:
                 title, username, password, note = row
                 self.db_handler.add_password(title, username, password, note)
